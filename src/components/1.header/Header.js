bbca8fc5af398d0bbf6049c5c5f4e00fd47f7e4e
redarashid <redarashid824@gmail.com>
2024-10-18 13:55:03 +0300

    first commit

--- src/components/1.header/Header.js
+++ src/components/1.header/Header.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 
 const Header = () => {
+
+  const[showModle, setShowModle] = useState(false)
+
   return (
-    <div>
-      Header
-    </div>
+    <header className='flex'>
+      <button onClick={()=> {
+        setShowModle(true)
+      }} className='menu icon-menu'></button>
+      <div/>
+
+      <nav className='navigate'>
+        <ul className='flex'>
+          <li> <a href='#about'>About</a> </li>
+          <li> <a href='#articles'>Articles</a> </li>
+          <li> <a href='#projects'>Projects</a> </li>
+          <li> <a href='#speaking'>Speaking</a> </li>
+          <li> <a href='#uses'>Uses</a> </li>
+        </ul>
+      </nav>
+
+      <button>
+        light
+      </button>
+
+
+      {showModle && (
+        <div className='fixed'>
+          <ul className='modle'>
+            <li><button className='icon-clos' onClick={()=> {
+              setShowModle(false)
+            }}></button></li>
+            <li> <a href='#about'>About</a> </li>
+            <li> <a href='#articles'>Articles</a> </li>
+            <li> <a href='#projects'>Projects</a> </li>
+            <li> <a href='#speaking'>Speaking</a> </li>
+            <li> <a href='#uses'>Uses</a> </li>
+          </ul>
+      </div>
+
+      )}
+
+    </header>
   )
 }
 
