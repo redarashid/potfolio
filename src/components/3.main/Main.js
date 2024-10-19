2b33f7cc8c6226f00ea044bd457c77a9dca0be3c
redarashid <redarashid824@gmail.com>
2024-10-19 22:38:07 +0300

    first commit

--- src/components/3.main/Main.js
+++ src/components/3.main/Main.js
@@ -13,7 +13,39 @@ const Main = () => {
       </section>
 
 
-      <section className='right-section'>Right</section>
+      <section className='flex right-section'>
+
+        {['aa', 'bb', 'cc', 'dd', 'ff', 'ee', 'ww', 'rr'].map((items)=> {
+          return (
+            <article key={items} className=' card'>
+          <img width={327} src='./1 (1).jpg' alt=''/>
+
+          <div className='box'>
+            <h1 className='title'>Landing Page 2</h1>
+            <p className='sup-title'>Lorem ipsum dolor sit amet consectetur elit<br/> adipisicing . Ex
+              tempore dolor in<br/> accusantium laudantium accusamus.
+            </p>
+
+            <div className='icons flex'>
+              <div style={{ gap: "11px" }} className="flex">
+                <i class="fa-solid fa-link"></i>
+                <i class="fa-brands fa-github"></i>
+              </div>
+
+              <a href='#more' className='link flex'>
+                More
+                <span style={{ alignSelf: "end" }}><i class="fa-solid fa-arrow-right"></i></span>
+              </a>
+            </div>
+          </div>
+        </article>
+          )
+        })}
+        
+
+        
+
+      </section>
     </main>
   )
 }
