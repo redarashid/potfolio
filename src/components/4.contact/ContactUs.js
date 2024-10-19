bb259832bd36b906fda9019cc2a6526f356f6446
redarashid <redarashid824@gmail.com>
2024-10-20 00:18:33 +0300

    first commit

--- src/components/4.contact/ContactUs.js
+++ src/components/4.contact/ContactUs.js
@@ -3,10 +3,36 @@ import './ContactUs.css'
 
 const ContactUs = () => {
   return (
-    <div>
-      ContactUs
-    </div>
-  )
+    <section className='contact-us'>
+      <h1 className='title'>
+        <i class="fa-solid fa-envelope"></i>
+        Contact US
+      </h1>
+      <p className='sup-title'>
+        Contact us for more information and Get notified when I publish
+        something new.
+      </p>
+
+      <div className='flex'>
+        <form className=''>
+
+          <div className='flex'>
+            <label htmlFor='email'>Email Address:</label>
+            <input placeholder='Email Address' required type='email' id='email'/>
+          </div>
+
+          <div className='flex' style={{marginTop: '24px'}}>
+            <label htmlFor='message'>Your Message:</label>
+            <textarea  placeholder='Your Message' name='' id='message'></textarea>
+          </div>
+
+          <button className="submit">Submit</button>
+        </form>
+
+        <div className='animation border'>Animation</div>
+      </div>
+    </section>
+  )  
 }
 
 export default ContactUs
